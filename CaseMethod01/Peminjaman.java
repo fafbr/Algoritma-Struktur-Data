b92147b300573b4eb49b12f69d13c6918f30d43d
fafa febriansa <fafafebrian11@gmail.com>
2026-04-14 22:59:29 +0700

    commit peminjaman class

--- CaseMethod01/Peminjaman.java
+++ CaseMethod01/Peminjaman.java
@@ -16,6 +16,14 @@ public class Peminjaman {
     public void hitungDenda() {
         if (this.lamaPinjam > this.batasPinjam) {
             this.terlambat = this.lamaPinjam - this.batasPinjam;
+            this.denda = this.terlambat * 2000;
+        } else {
+            this.terlambat = 0;
+            this.denda = 0;
         }
+    }
+
+    public void tampilPeminjaman() {
+        System.out.printf("| %-7s | %-15s | %-11d | %-9d | Rp %-7d |\n",mhs.nama, buku.judul, lamaPinjam, terlambat, denda);
     }
 }
